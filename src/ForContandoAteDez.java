0fdc970bb7a257dff9e27e07068267c3848fa25b
Laiane Karla <lannekarla03@gmail.com>
2022-05-24 21:14:38 -0300

    Praticando laços condicionais e laços de repetição

--- src/ForContandoAteDez.java
+++ src/ForContandoAteDez.java
@@ -1,9 +1,21 @@
+/*Escreva um programa Java que usa o laço for para contar de 0 até 10 e somar todos estes valores, 
+ou seja, 0 + 1 + 2 + 3 + etc. Como resultado seu programa deverá exibir o valor 55*/
+
+
+
+import java.util.Scanner;
 
 public class ForContandoAteDez {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
 	}
 
 }
